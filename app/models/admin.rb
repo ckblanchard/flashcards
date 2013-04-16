2370422040625bc75878948cb6d9ce4cdf06dba3
Craig Blanchard <craig@ckbgraphics.com>
2013-04-15 21:20:20 -0700

    Fixed admin registration creating organization name

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,25 +5,37 @@ class Admin < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  after_initialize :initialize_organization
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :organization_id, :organization_name
+  attr_accessible :email, :password, :password_confirmation, 
+    :remember_me, :first_name, :last_name, :organization_attributes
   # attr_accessible :title, :body
 
   belongs_to :organization
+  accepts_nested_attributes_for :organization
 
-  # After create takes a symbol for the name of a method it should call when the time comes
-  after_create :add_to_organization
+  # # After create takes a symbol for the name of a method it should call when the time comes
+  # after_create :add_to_organization
 
-  # Our method that after_create will call when something has been created
-  def add_to_organization
-    organization = Organization.create(name: self.organization_name) 
-    # Was setting it to the email because organizations have a validation on the name
+  # # Our method that after_create will call when something has been created
+  # def add_to_organization
+  #   organization = Organization.create!(name: self.organization_name) 
+  #   # Was setting it to the email because organizations have a validation on the name
  
-    # Assign the newly created organization to this user object
-    self.organization = organization
+  #   # Assign the newly created organization to this user object
+  #   self.organization = organization
  
-    # Once we assign the association, save it!
-    self.save
+  #   # Once we assign the association, save it!
+  #   self.save
+  # end
+
+  private
+
+  def initialize_organization
+    if new_record? && !organization
+      build_organization
+    end
   end
 
 end
