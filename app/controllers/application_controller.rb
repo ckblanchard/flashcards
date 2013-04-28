2ce567509a78871dd1292cd6355a6876f3be963f
Craig Blanchard <craig@ckbgraphics.com>
2013-04-27 23:45:43 -0700

    undo application controller user redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def after_sign_in_path_for(resource)
-    if resource.is_a?(User)
-      "/decks"
-    else
-      super
-    end
-  end
+#   def after_sign_in_path_for(resource)
+#     if resource.is_a?(User)
+#       "/decks"
+#     else
+#       super
+#     end
+#   end
 
   protected
   def authenticate_inviter!
